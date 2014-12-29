71283a6ada7d41e68cef0707167febe9387ffb48
Krzysztof Gzocha <kgzocha@gmail.com>
2014-12-29 11:49:55 +0100

    Tested real example in SignatureCalculator

---
+++ spec/Team3/SignatureCalculator/ParametersSorter/ParametersSorterSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\Team3\SignatureCalculator\ParametersSorter;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Team3\Order\Model\OrderInterface;
+use Team3\Order\Serializer\SerializerInterface;
+
+class ParametersSorterSpec extends ObjectBehavior
+{
+    const EXAMPLE_STRING = 'someString';
+
+    public function let(
+        SerializerInterface $serializer,
+        OrderInterface $order
+    ) {
+        $serializer->toJson($order)->willReturn(self::EXAMPLE_STRING);
+        $serializer->fromJson(self::EXAMPLE_STRING, 'array')->willReturn([]);
+
+        $this->beConstructedWith($serializer);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Team3\SignatureCalculator\ParametersSorter\ParametersSorter');
+    }
+
+    public function it_should_return_array(
+        OrderInterface $order
+    ) {
+        $this->getSortedParameters($order)->shouldBeArray();
+    }
+}
