6b1ae3997ac96bf131eedc8b0593946d2e39867c
Krzysztof Gzocha <kgzocha@gmail.com>
2015-01-13 17:06:57 +0100

    Moved test folder

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,13 +3,13 @@
 
 // Register annotations
 \Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
-    __DIR__.'/../Team3/Annotation/PayU.php'
+    __DIR__.'/../src/Team3/Annotation/PayU.php'
 );
 \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
     'JMS\Serializer\Annotation',
-    __DIR__.'/../../vendor/jms/serializer/src'
+    __DIR__.'/../vendor/jms/serializer/src'
 );
 \Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
     'Symfony\\Component\\Validator',
-    __DIR__.'/../../vendor/symfony/validator'
+    __DIR__.'/../vendor/symfony/validator'
 );
